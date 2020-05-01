f5d935a38b9e69a3b77919d5ffffa67418559b8a
ThinkingDove <396593090@qq.com>
2020-05-01 19:06:59 +0800

    springcloud p8

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther lirenxiang
+ * @date 2020/5/1 18:39
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
