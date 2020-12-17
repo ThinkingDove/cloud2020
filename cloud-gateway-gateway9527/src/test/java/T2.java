ef800370006e2206ebce1ec0444c77e00ea2ee20
ThinkingDove <396593090@qq.com>
2020-12-17 21:41:48 +0800

    p72 GateWay常用的Predicate

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author lirenxiang
+ * @date 2020/12/17 21:09
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+
+//        2020-12-17T21:10:23.234+08:00[Asia/Shanghai]
+    }
+}
