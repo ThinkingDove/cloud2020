8f01f2bcbfa365eb110d47fc3b76ca8a1c57d8ee
ThinkingDove <396593090@qq.com>
2021-01-30 17:33:31 +0800

    p104 Nacos之Group配置

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lirenxiang
+ * @date 2021/1/30 16:21
+ */
+@RestController
+@RefreshScope //支持Nacos的动态刷新功能
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
